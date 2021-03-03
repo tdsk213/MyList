9f406e12d3b7c979b6c886e0a9651731b8ec7b74
tdsk213 <tdsk213@ya.ru>
2021-03-03 14:00:04 +0400

    sort method test1 Object

--- src/main/java/ru/madbrains/javacourse/homework/part1/MyList.java
+++ src/main/java/ru/madbrains/javacourse/homework/part1/MyList.java
@@ -6,12 +6,26 @@ public class MyList<T> implements AdvancedList<T>, AuthorHolder {
 
     private int maxCapacity = 10;
     private int size = 0;
+    private static final int MIN_MERGE = 32;
 
 
     private Object[] elements;
 
     public MyList() {
         this.elements = new Object[maxCapacity];
+    }
+
+    private static boolean compare(Object s1, Object s2) {
+
+        if (new Comparator<Object>() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return ((Comparable)o1).compareTo((Comparable)o2);
+            }
+        }.compare(s1, s2) >= 0) {
+            return true;
+        } else
+            return false;
     }
 
     @Override
@@ -37,7 +51,102 @@ public class MyList<T> implements AdvancedList<T>, AuthorHolder {
 
     @Override
     public AdvancedList<T> sort(Comparator<T> comparator) {
+
+        AdvancedList<T> sortedList = new MyList<>();
+        Object[] sortedArray = new Object[size];
+        timSort(sortedArray);
+
         return null;
+    }
+
+    private static int getMinrun(int n)
+    {
+        assert n >= 0;
+
+        int r = 0;
+        while (n >= MIN_MERGE)
+        {
+            r |= (n & 1);
+            n >>= 1;
+        }
+        return n + r;
+    }
+
+    private static void insertionSort(Object[] arr, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            Object temp = arr[i];
+            int j = i - 1;
+            while (j >= left && compare(arr[j], temp)) //arr[j] > temp
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+    }
+
+    private static void merge(Object[] arr, int l, int m, int r) {
+
+        int len1 = m - l + 1, len2 = r - m;
+        Object[] left = new Object[len1];
+        Object[] right = new Object[len2];
+
+        for (int x = 0; x < len1; x++) {
+            left[x] = arr[l + x];
+        }
+
+        for (int x = 0; x < len2; x++) {
+            right[x] = arr[m + 1 + x];
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = l;
+
+        while (i < len1 && j < len2) {
+            if (compare(left[i], right[j])) { //left[i] <= right[j]
+                arr[k] = left[i];
+                i++;
+            }
+            else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < len1) {
+            arr[k] = left[i];
+            k++;
+            i++;
+        }
+
+        while (j < len2) {
+            arr[k] = right[j];
+            k++;
+            j++;
+        }
+    }
+
+    public static void timSort(Object[] arr) {
+        int n = arr.length;
+        int minRun = getMinrun(MIN_MERGE);
+
+        for (int i = 0; i < n; i += minRun) {
+            insertionSort(arr, i, Math.min((i + 31), (n - 1)));
+        }
+
+        for (int size = minRun; size < n; size = 2 * size) {
+
+            for (int left = 0; left < n; left += 2 * size) {
+
+                int mid = left + size - 1;
+                int right = Math.min((left + 2 * size - 1),
+                        (n - 1));
+
+                merge(arr, left, mid, right);
+            }
+        }
     }
 
     @Override
