0319e6604ad18a5143890b1215287d2b70c21f25
tdsk213 <tdsk213@ya.ru>
2021-03-03 13:07:31 +0400

    TimSort String test

--- src/main/java/ru/madbrains/javacourse/homework/part1/ru/madbrains/javacourse/TimSort.java
+++ src/main/java/ru/madbrains/javacourse/homework/part1/ru/madbrains/javacourse/TimSort.java
@@ -1,14 +1,25 @@
 package ru.madbrains.javacourse.homework.part1.ru.madbrains.javacourse;
 
+import java.util.Comparator;
+import java.util.Map;
+
 public class TimSort {
 
     private static final int MIN_MERGE = 32;
+
+    private static Comparator<String> getComparator() {
+        return new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        };
+    }
 
     public static int getMinrun(int n)
     {
         assert n >= 0;
 
-        // Becomes 1 if any 1 bits are shifted off
         int r = 0;
         while (n >= MIN_MERGE)
         {
@@ -18,11 +29,9 @@ public class TimSort {
         return n + r;
     }
 
-    // This function sorts array from left index to
-// to right index which is of size atmost RUN
     public static void insertionSort(Integer[] arr, int left, int right) {
         for (int i = left + 1; i <= right; i++) {
-            int temp = arr[i];
+            Integer temp = arr[i];
             int j = i - 1;
             while (j >= left && arr[j] > temp)
             {
@@ -35,8 +44,7 @@ public class TimSort {
 
     // Merge function merges the sorted runs
     public static void merge(Integer[] arr, int l, int m, int r) {
-        // Original array is broken in two parts
-        // left and right array
+
         int len1 = m - l + 1, len2 = r - m;
         int[] left = new int[len1];
         int[] right = new int[len2];
@@ -53,8 +61,6 @@ public class TimSort {
         int j = 0;
         int k = l;
 
-        // After comparing, we merge those two array
-        // in larger sub array
         while (i < len1 && j < len2) {
             if (left[i] <= right[j]) {
                 arr[k] = left[i];
@@ -67,16 +73,12 @@ public class TimSort {
             k++;
         }
 
-        // Copy remaining elements
-        // of left, if any
         while (i < len1) {
             arr[k] = left[i];
             k++;
             i++;
         }
 
-        // Copy remaining element
-        // of right, if any
         while (j < len2) {
             arr[k] = right[j];
             k++;
@@ -84,48 +86,27 @@ public class TimSort {
         }
     }
 
-    // Iterative Timsort function to sort the
-// array[0...n-1] (similar to merge sort)
     public static void timSort(Integer[] arr) {
         int n = arr.length;
         int minRun = getMinrun(MIN_MERGE);
 
-        // Sort individual subarrays of size RUN
         for (int i = 0; i < n; i += minRun) {
             insertionSort(arr, i, Math.min((i + 31), (n - 1)));
         }
 
-        // Start merging from size
-        // RUN (or 32). It will
-        // merge to form size 64,
-        // then 128, 256 and so on
-        // ....
         for (int size = minRun; size < n; size = 2 * size) {
 
-            // Pick starting point
-            // of left sub array. We
-            // are going to merge
-            // arr[left..left+size-1]
-            // and arr[left+size, left+2*size-1]
-            // After every merge, we
-            // increase left by 2*size
             for (int left = 0; left < n; left += 2 * size) {
 
-                // Find ending point of left sub array
-                // mid+1 is starting point of right sub
-                // array
                 int mid = left + size - 1;
                 int right = Math.min((left + 2 * size - 1),
                         (n - 1));
 
-                // Merge sub array arr[left.....mid] &
-                // arr[mid+1....right]
                 merge(arr, left, mid, right);
             }
         }
     }
 
-    // Utility function to print the Array
     public static void printArray(Integer[] arr, int n) {
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
